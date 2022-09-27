b166d5a3d6a2339a622a1be41811462ac9ce2e14
Rrudger <soulo@yandex.ru>
2022-09-27 15:56:15 +0200

    pass 3

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Rrudger/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Rrudger/frontend-project-lvl2/actions)
+
+ asciinema:
+ https://asciinema.org/a/sxW9l6ugW8Zl1oGgVBqybosR2
--- gendiff.js
+++ gendiff.js
@@ -1,13 +1,39 @@
-const { program } = require('commander');
+#!/usr/bin/env node
+import { program } from 'commander';
+import { readFileSync } from 'node:fs';
+import {resolve} from 'node:path';
+import { cwd } from 'node:process';
 
 program
   .name('gendiff')
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>');
+  .argument('<filepath1> <filepath2>')
+  .action(() => {
+    console.log(genDiff(program.args[0], program.args[1]));
+  });;
 
 program.parse(process.argv);
 
-const options = program.opts();
-console.log(program.args);
+export default function genDiff(path1, path2) {
+  const pathStr1 = path1.split('/');
+  const pathStr2 = path2.split('/');
+  const data1 = JSON.parse(readFileSync(resolve(cwd(), 'files', pathStr1[pathStr1.length - 1]), 'utf-8'));
+  const data2 = JSON.parse(readFileSync(resolve(cwd(), 'files', pathStr2[pathStr2.length - 1]), 'utf-8'));
+  const keysArr = [... new Set([...Object.keys(data1), ...Object.keys(data2)])].sort();
+  const result = {};
+  keysArr.forEach((keyword) => {
+    if (data1[keyword] === data2[keyword]) {
+      result[keyword] = data1[keyword];
+    } else if (data1[keyword] === undefined){
+      result[`+ ${keyword}`] = data2[keyword];
+    } else if (data2[keyword] === undefined){
+      result[`- ${keyword}`] = data1[keyword]
+    } else {
+      result[`- ${keyword}`] = data1[keyword];
+      result[`+ ${keyword}`] = data2[keyword];
+    };
+  });
+  return result;
+}
