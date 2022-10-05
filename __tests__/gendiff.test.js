cbb9b2fee35d0b8d49115206c6f42f2232c7d2a8
Rrudger <soulo@yandex.ru>
2022-10-05 23:16:44 +0200

    update github actions

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,6 +3,5 @@ import { readFileSync } from 'node:fs';
 
 test('test for test', () => {
   const diffData = readFileSync('./__fixtures__/fileDiff.txt', 'utf-8');
-  console.log(diffData);
   expect(genDiff('file1.json', 'file2.json').trim()).toMatch(diffData.trim());
 });
