0edb49392e1ed3ad64e318b06e5364dd592fa297
Rrudger <soulo@yandex.ru>
2022-10-06 15:45:15 +0200

    refactoring tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,12 +1,11 @@
 import genDiff from '../src/gendiffFunc.js';
 import { readFileSync } from 'node:fs';
 
+const diffData = readFileSync('./__fixtures__/fileDiff.txt', 'utf-8');
 test('json test', () => {
-  const diffData = readFileSync('./__fixtures__/fileDiff.txt', 'utf-8');
   expect(genDiff('file1.json', 'file2.json').trim()).toMatch(diffData.trim());
 });
 
 test('yaml test', () => {
-  const diffData = readFileSync('./__fixtures__/fileDiff.txt', 'utf-8');
   expect(genDiff('file1.yml', 'file2.yml').trim()).toMatch(diffData.trim());
 });
