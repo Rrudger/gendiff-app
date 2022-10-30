696515d2626dca5edacca50ba37ee940d76519e8
Rrudger <soulo@yandex.ru>
2022-10-30 21:08:13 +0100

    update autocheck

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -38,6 +38,7 @@ export default function stylishFormater(obj) {
       return key;
   }
   });
-  const resStr = `${terzArr.join('').slice(0, -3).trim()}\n}`;
+  const resStr = `${terzArr.join('').slice(0, -2).trim()}\n}`;
+  writeFileSync('./test_fixtures/try.txt', resStr);
   return resStr;
 }
