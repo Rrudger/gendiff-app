65b00d5bfcc8edbcb6432d0d84598d912a3304e9
Rrudger <soulo@yandex.ru>
2022-10-31 16:08:43 +0100

    update linter autocheck

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -17,13 +17,11 @@ export default function stylishFormater(obj) {
     }
   });
   const secArr = primArr.join('').split('').filter((key) => key !== ',').map((key, index, arr) => {
-    switch (key) {
-      case '\n':
-        const tabCount = (arr.slice(0, index).filter((k) => k === '{').length) - (arr.slice(0, index).filter((k) => k === '}').length);
-        return tabCount > 1 ? `\n${_.repeat('  ', tabCount + (tabCount - 1))}` : '\n  ';
-      default:
-        return key;
+    if (key === '\n') {
+      const tabCount = (arr.slice(0, index).filter((k) => k === '{').length) - (arr.slice(0, index).filter((k) => k === '}').length);
+      return tabCount > 1 ? `\n${_.repeat('  ', tabCount + (tabCount - 1))}` : '\n  ';
     }
+    return key;
   });
   const terzArr = secArr.join('').split('').filter((key, index, arr) => (arr[index + 1] !== '}' && arr[index + 2] !== '}') || index + 1 === arr.length - 1);
   const resStr = `${terzArr.join('').slice(0, -2).trim()}\n}`;
