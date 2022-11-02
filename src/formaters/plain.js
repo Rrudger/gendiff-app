b4deeabb97954d26312a6df16af93631b514e0c4
Rrudger <soulo@yandex.ru>
2022-11-03 00:38:45 +0100

    update linter autocheck

--- src/formaters/plain.js
+++ src/formaters/plain.js
@@ -9,45 +9,30 @@ function checkValue(value) {
   return typeof (value) === 'string' ? `'${value}'` : value;
 }
 
-function plainRec(obj, path, result) {
-  const keys = Object.keys(obj);
-  for (let i = 0; i < keys.length; i += 1) {
-    if ((typeof (obj[keys[i]]) !== 'object') || (obj[keys[i]]) === null) {
-      if ((i < keys.length - 1) && (keys[i].slice(2) === keys[i + 1].slice(2))) {
-        const newValue = `Property '${getPath(path, keys[i])}' was updated. From ${checkValue(obj[keys[i]])} to ${checkValue(obj[keys[i + 1]])}`;
-      //result = [ ...result, newValue];
-        //console.log();
-        //console.log(testresult);
-        result.push(`Property '${getPath(path, keys[i])}' was updated. From ${checkValue(obj[keys[i]])} to ${checkValue(obj[keys[i + 1]])}`);
-        //console.log(result);
-        i += 1;
-      } else if ((keys[i][0] === '+')) {
-        result.push(`Property '${getPath(path, keys[i])}' was added with value: ${checkValue(obj[keys[i]])}`);
-      } else if (keys[i][0] === '-') {
-        result.push(`Property '${getPath(path, keys[i])}' was removed`);
+function plainRec(obj, path) {
+  return Object.keys(obj).reduce((resArr, key, index, array) => {
+    if (key[0] === '-') {
+      if ((index < array.length - 1) && (key.slice(2) === array[index + 1].slice(2))) {
+        return [...resArr, `Property '${getPath(path, key)}' was updated. From ${checkValue(obj[key])} to ${checkValue(obj[array[index + 1]])}`];
       }
-    } else if ((i < keys.length - 1) && (keys[i].slice(2) === keys[i + 1].slice(2))) {
-      const value1 = (typeof (obj[keys[i]]) === 'object' ? '[complex value]' : checkValue(obj[keys[i]]));
-      const value2 = (typeof (obj[keys[i + 1]]) === 'object' ? '[complex value]' : checkValue(obj[keys[i + 1]]));
-      result.push(`Property '${getPath(path, keys[i])}' was updated. From ${value1} to ${value2}`);
-      i += 1;
-    } else if (keys[i][0] === '+') {
-      result.push(`Property '${getPath(path, keys[i])}' was added with value: [complex value]`);
-    } else if (keys[i][0] === '-') {
-      result.push(`Property '${getPath(path, keys[i])}' was removed`);
-    } else {
-      const newPath = [...path, keys[i].trim()];
-      //newPath.push(keys[i].trim());
-      plainRec(obj[keys[i]], newPath, result);
+      return [...resArr, `Property '${getPath(path, key)}' was removed`];
     }
-    if (i === keys.length - 1) path.pop();
-  }
+    if (key[0] === '+') {
+      if ((index === 0) || (key.slice(2) !== array[index - 1].slice(2))) {
+        return [...resArr, `Property '${getPath(path, key)}' was added with value: ${checkValue(obj[key])}`];
+      }
+    }
+    if ((typeof (obj[key]) === 'object') && (obj[key]) !== null) {
+      const newPath = [...path, key.trim()];
+      return [...resArr, ...plainRec(obj[key], newPath)];
+    }
+    return resArr;
+  }, []);
 }
 
 export default function plainFormater(obj) {
-  const result = [];
   const path = [];
-  plainRec(obj, path, result);
+  const result = plainRec(obj, path);
   return result.map((str) => str.includes('object') ? _.replace(str, 'object Object', 'complex value') : str)
     .join('\n');
 }
