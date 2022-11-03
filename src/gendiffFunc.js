9f492e4e4e26d2ae6c7eaa72c1691180dbc7b515
Rrudger <soulo@yandex.ru>
2022-11-04 00:32:58 +0100

    update linter autockeck

--- src/gendiffFunc.js
+++ src/gendiffFunc.js
@@ -10,62 +10,43 @@ const formaters = {
   json: jsonFormater,
 };
 
-function addRec(obj, resultObj) {
-  Object.keys(obj).forEach((key) => {
+function addTab(obj) {
+  return Object.keys(obj).reduce((resObj, key) => {
     if ((typeof (obj[key]) !== 'object') || ((obj[key]) === null)) {
       const tempKey = (key[0] === '+' || key[0] === '-') ? key : `  ${key}`;
-      resultObj[tempKey] = obj[key];
-    } else {
-      const temp = (key[0] === '+' || key[0] === '-') ? key : `  ${key}`;
-      resultObj[temp] = {};
-      addRec(obj[key], resultObj[temp]);
+      return { ...resObj, [tempKey]: obj[key] };
     }
-  });
+    const temp = (key[0] === '+' || key[0] === '-') ? key : `  ${key}`;
+    return { ...resObj, [temp]: addTab(obj[key]) };
+  }, {});
 }
 
-function addTab(obj) {
-  const result = {};
-  addRec(obj, result);
-  return result;
-}
-
-function recDiff(obj1, obj2, resObj) {
-  const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])].sort();
-  keys.forEach((k) => {
+function recDiff(obj1, obj2) {
+  const keys = _.sortBy([...new Set([...Object.keys(obj1), ...Object.keys(obj2)])]);
+  return keys.reduce((resObj, k) => {
     if (((typeof (obj1[k]) !== 'object') || (typeof (obj2[k]) !== 'object'))
-     || obj1[k] === null || obj2[k] === null) {
+    || obj1[k] === null || obj2[k] === null) {
       if (obj1[k] === obj2[k]) {
-        resObj[k] = obj1[k];
-      } else if (obj1[k] === undefined) {
-        resObj[`+ ${k}`] = obj2[k];
-      } else if (obj2[k] === undefined) {
-        resObj[`- ${k}`] = obj1[k];
-      } else {
-        resObj[`- ${k}`] = obj1[k];
-        resObj[`+ ${k}`] = obj2[k];
+        return { ...resObj, [k]: obj1[k] };
       }
-    } else {
-      let tempKey = k;
-      const tempObj1 = obj1[k];
-      const tempObj2 = obj2[k];
       if (obj1[k] === undefined) {
-        tempKey = `+ ${k}`;
-        resObj[tempKey] = _.cloneDeep(obj2[k]);
-      } else if (obj2[k] === undefined) {
-        tempKey = `- ${k}`;
-        resObj[tempKey] = _.cloneDeep(obj1[k]);
-      } else {
-        resObj[tempKey] = {};
-        recDiff(tempObj1, tempObj2, resObj[tempKey]);
+        return { ...resObj, [`+ ${k}`]: obj2[k] };
       }
+      if (obj2[k] === undefined) {
+        return { ...resObj, [`- ${k}`]: obj1[k] };
+      }
+      return { ...resObj, [`- ${k}`]: obj1[k], [`+ ${k}`]: obj2[k] };
     }
-  });
+    if (obj1[k] === undefined) {
+      return { ...resObj, [`+ ${k}`]: _.cloneDeep(obj2[k]) };
+    }
+    if (obj2[k] === undefined) {
+      return { ...resObj, [`- ${k}`]: _.cloneDeep(obj1[k]) };
+    }
+    return { ...resObj, [k]: recDiff(obj1[k], obj2[k]) };
+  }, {});
 }
 
 export default function genDiff(path1, path2, format = 'stylish') {
-  const data1 = parseFile(path1);
-  const data2 = parseFile(path2);
-  const result = {};
-  recDiff(data1, data2, result);
-  return formaters[format](addTab(result));
+  return formaters[format](addTab(recDiff(parseFile(path1), parseFile(path2))));
 }
